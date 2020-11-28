6e6c547a3a8abcfbb52959eecd9c93170f78b014
Osman Elsahib <elsahib10@gmail.com>
2020-11-28 18:40:03 +0000

    finished all

--- TechReturnersExercises/Exercise002.cs
+++ TechReturnersExercises/Exercise002.cs
@@ -4,15 +4,17 @@ namespace TechReturners.Exercises
 {
     public class Exercise002
     {
-        public static bool IsFromManchester(/* Person person */) 
+        public static bool IsFromManchester(Person person) 
         {
             // Add your code here!
+            return person.City == "Manchester";
             throw new NotImplementedException();
         }
 
-        public static bool CanWatchFilm(/*Person person, int ageLimit*/) 
+        public static bool CanWatchFilm(Person person, int ageLimit) 
         {
             // Add your code here!
+            return person.Age > ageLimit;
             throw new NotImplementedException();
         }
     }
